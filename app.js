302d76b33218ee8ac74c1a850b61d0a04577349b
Tomas Carrizo <tomascarrizo.dev@gmail.com>
2023-05-23 13:53:29 -0300

    Mejora de codigo y nueva funcion de creacion y eliminacion de proyectos

--- app.js
+++ app.js
@@ -5,13 +5,14 @@ const $aside = document.querySelector('#aside');
 
 const $ownerName = document.querySelector('#owner-container-name')
 
+const $deleteContainer = document.querySelector('#delete-project-container');
+
 //* Funcional elements
 const $nameCardie = document.querySelector('#name-input');
 const $projectNameCardie = document.querySelector('#name-project-input');
 const $btnStart = document.querySelector('#btn-start');
 
 const $btnNewProject = document.querySelector('#btn-new-project');
-
 
 let menuValue = true
 const $btnMenu = document.querySelector('#btn-menu');
@@ -20,20 +21,40 @@ const $svgCloseMenu = document.querySelector('#btn-close-menu');
 
 const $projectContainer = document.querySelector('#project-container');
 
+let $deleteProjectName = document.querySelector('#delete-project-name')
+const $cancelDeleteProject = document.querySelector('#delete-project-btn-cancel')
+const $confirmDeleteProject = document.querySelector('#delete-project-btn-confirm')
+
 //* Variables
+const closeSvg = `
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+        <path
+            d = "M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+    </svg > 
+`
+
 let projectCounter = 0
 let projectCurrent
-
+const regex = /^[a-zA-Z 0-9!?,.:;]*[a-zA-Z ][a-zA-Z 0-9!?,.:;]*$/
 let valueNewInput
+let creating = false
 
 let projectsCreated = []
+let projectsCreatedArr = []
+
+let deletingProject
+let deleteProjectID
 
 //* Visual functions
+function newElement(elem) {
+    return document.createElement(elem)
+}
+
 function start() {
-    let owner = $nameCardie.value
-    let projectName = $projectNameCardie.value
-    //! detectar espacios vacios
-    if (owner !== '' && projectName !== '' && owner !== ' ' && projectName !== ' ') {
+    let owner = $nameCardie.value.trim()
+    let projectName = $projectNameCardie.value.trim()
+
+    if (owner.match(regex) && projectName.match(regex)) {
         // animacion del start al main
         $welcome.classList.add('disapear')
 
@@ -48,22 +69,45 @@ function start() {
         }, 300)
 
         // creaccion del project en el menu
-        $projectContainer.innerHTML += `
-        <div class="project project-on">
-            <button id="btn-project-name" class="menu-project">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                    <path
-                        d="M495.9 166.6c3.2 8.7 .5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3 39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1 31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2 17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4L83.1 425.9c-8.8 2.8-18.6 .3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4C64.6 273.1 64 264.6 64 256s.6-17.1 1.7-25.4L22.4 191.2c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1 9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7 8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336a80 80 0 1 0 0-160 80 80 0 1 0 0 160z" />
-                </svg>
-            </button>
-            <span id="aside-project-name">${projectName}</span>
-        </div>
-        `
+
+        // $projectContainer.innerHTML += `
+        // <div class="project project-on">
+        //     <button id="btn-${projectCounter}" class="menu-project" onclick="showDeleteProjectMenu">
+        //         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+        //             <path
+        //                 d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+        //         </svg>
+        //     </button>
+        //     <span id="project-${projectCounter}">${projectName}</span>
+        // </div>
+        // `
+
+        
+        let div = newElement('div')
+        div.setAttribute('class', 'project project-on')
+        div.setAttribute('id', `project-${projectCounter}`)
+
+        let button = newElement('button')
+        button.setAttribute('id', `btn-${projectCounter}`)
+        button.setAttribute('class', 'menu-project')
+        button.setAttribute('onclick', 'showDeleteProjectMenu()')
+
+        let span = newElement('span')
+        span.setAttribute('id', `project-${projectCounter}`)
+        span.textContent = projectName
+
+        button.innerHTML = closeSvg
+        div.appendChild(button)
+        div.appendChild(span)
+
+        $projectContainer.append(div)
+
         projectsCreated.push(projectName)
-        $ownerName.innerHTML = owner + "'s Cardie"
+        projectsCreatedArr.push(div)
+        $ownerName.innerHTML = owner.charAt(0).toUpperCase() + "'s Cardie"
 
         projectCurrent = projectName
-
+        projectCounter++
     } else {
         alert('Campos incorrectos o incompletos')
     }
@@ -84,25 +128,54 @@ function toggleMenu() {
 }
 
 function newProject() {
-    $projectContainer.innerHTML += `
-        <div class="project" id="new-project-temp">
-            <input type="text" id="input-new-project">
-            <button id="confirm-new-project" onclick="confirmNewProject()">Create</button>
-            <button id="cancel-new-project" onclick="cancelNewProject()">Cancel</button>
-        </div>
-        `
-        let $input = document.querySelector('#input-new-project');
-        console.log($input.value)
-}
+    if (creating == false) {
+        // $projectContainer.innerHTML += `
+        //     <div class="project" id="new-project-temp">
+        //         <input type="text" id="input-new-project">
+        //         <button id="confirm-new-project" onclick="confirmNewProject()">Create</button>
+        //         <button id="cancel-new-project" onclick="cancelNewProject()">Cancel</button>
+        //     </div>
+        //     `
+        
+        let div = newElement('div')
+        div.setAttribute('class', 'project')
+        div.setAttribute('id', 'new-project-temp')
 
+        let input = newElement('input')
+        input.setAttribute('type', 'text')
+        input.setAttribute('id', 'input-new-project')
+
+        let btnConfirm = newElement('button')
+        btnConfirm.setAttribute('id', 'confirm-new-project')
+        btnConfirm.textContent = 'Confirm'
+        btnConfirm.setAttribute('onclick', `confirmNewProject()`)
+        
+        let btnCancel = newElement('button')
+        btnCancel.setAttribute('id', 'cancel-new-project')
+        btnCancel.textContent = 'Cancel'
+        btnCancel.setAttribute('onclick', `cancelNewProject()`)
+        
+        div.appendChild(input)
+        div.appendChild(btnConfirm)
+        div.appendChild(btnCancel)
+
+        $projectContainer.appendChild(div)
+
+        input.focus()
+        
+        creating = true
+    } else {
+        return false
+    }
+}
 
 function confirmNewProject() {
     let $input = document.querySelector('#input-new-project');
     let repeated = false
-    if ($input.value === '' || $input.value === " ") {
+    if (!$input.value.match(regex)) {
         alert('Ingresa un texto valido')
     } else {
-        for (let i = 0; i < projectsCreated.length; i++ ) {
+        for (let i = 0; i < projectsCreated.length; i++) {
             if ($input.value == projectsCreated[i]) {
                 repeated = true
             }
@@ -113,28 +186,65 @@ function confirmNewProject() {
             alert('Proyecto ya creado')
         }
     }
-    console.log($input.value)
 }
 
 function cancelNewProject() {
     let $newProject = document.querySelector('#new-project-temp');
     $projectContainer.removeChild($newProject)
+    creating = false
 }
 
 function createNewProject(input) {
-    $projectContainer.innerHTML += `
-        <div class="project">
-            <button id="btn-project-name" class="menu-project">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                    <path
-                        d="M495.9 166.6c3.2 8.7 .5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3 39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1 31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2 17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4L83.1 425.9c-8.8 2.8-18.6 .3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4C64.6 273.1 64 264.6 64 256s.6-17.1 1.7-25.4L22.4 191.2c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1 9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7 8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336a80 80 0 1 0 0-160 80 80 0 1 0 0 160z" />
-                </svg>
-            </button>
-            <span id="aside-project-name">${input.value}</span>
-        </div>
-        `
-        projectsCreated.push(input.value)
-        cancelNewProject()
+    // $projectContainer.innerHTML += `
+    //     <div class="project">
+    //         <button id="btn-${projectCounter}" class="menu-project">
+    //         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+    //         <path
+    //             d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+    //     </svg>
+    //         </button>
+    //         <span id="project-${projectCounter}">${input.value}</span>
+    //     </div>
+    //     `
+
+    let div = newElement('div')
+    div.setAttribute('class', 'project')
+    div.setAttribute('id', `project-${projectCounter}`)
+
+    let button = newElement('button')
+    button.setAttribute('id', `btn-${projectCounter}`)
+    button.setAttribute('class', 'menu-project')
+    button.setAttribute('onclick', 'showDeleteProjectMenu()')
+
+    let span = newElement('span')
+    span.setAttribute('id', `project-${projectCounter}`)
+    span.textContent = input.value
+
+    button.innerHTML = closeSvg
+    div.appendChild(button)
+    div.appendChild(span)
+
+    $projectContainer.append(div)
+
+    projectCounter++
+    projectsCreated.push(input.value)
+    projectsCreatedArr.push(div)
+    cancelNewProject()
+}
+
+function showDeleteProjectMenu() {
+    $deleteContainer.style = 'display: block;'
+    // console.log($deleteContainer.querySelector('span'))
+}
+
+function cancelDeleteProject() {
+    $deleteContainer.style = 'display: none;'
+}
+
+function confirmDeleteProject() {
+    console.log(deleteProjectID)
+    document.querySelector(`#${deleteProjectID}`).remove()
+    cancelDeleteProject()
 }
 
 //* Event listeners
@@ -143,3 +253,44 @@ $btnStart.addEventListener('click', start)
 $btnMenu.addEventListener('click', toggleMenu)
 
 $btnNewProject.addEventListener('click', newProject)
+
+// $projectContainer.addEventListener('change', event => {
+//     projectsCreatedArr = document.querySelectorAll('.project').filter
+// })
+
+$projectContainer.addEventListener('click', event => {
+    let elem = event.target
+    let deleteName
+    let deleteID
+    if (elem.tagName == 'path') {
+        deleteName = elem.parentNode.parentNode.parentNode.querySelector('span').textContent
+        deleteID = elem.parentNode.parentNode.parentNode.id
+        // console.log(deleteName)
+        // console.log(deleteID)
+    }
+    else if (elem.tagName == 'svg') {
+        deleteName = elem.parentNode.parentNode.querySelector('span').textContent
+        deleteID = elem.parentNode.parentNode.id
+        // console.log(deleteName)
+        // console.log(deleteID)
+    } 
+    else if (elem.tagName == 'button') {
+        deleteName = elem.parentNode.querySelector('span').textContent
+        deleteID = elem.parentNode.id
+        // console.log(deleteName)
+        // console.log(deleteID)
+    }
+
+    $deleteProjectName.textContent = deleteName
+    deleteProjectID = deleteID
+})
+
+// $btnDeleteProjectMenu.addEventListener('click', event => {
+//     $deleteContainer.style = 'display: block;'
+//     console.log($deleteContainer)
+// })
+
+
+$cancelDeleteProject.addEventListener('click', cancelDeleteProject) 
+
+$confirmDeleteProject.addEventListener('click', confirmDeleteProject)
